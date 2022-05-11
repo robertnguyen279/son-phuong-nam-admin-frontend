245d4dd55c8e324757734a214af36211287d2f8f
Robert Nguyen <robert.nguyen279@gmail.com>
2022-05-11 11:59:19 +0700

    Change to production mode

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
-const baseURL = 'https://tcbg3jx755.execute-api.ap-southeast-1.amazonaws.com';
+// dev
+// const baseURL = 'https://tcbg3jx755.execute-api.ap-southeast-1.amazonaws.com';
+
+// prod
+const baseURL = 'https://bdxuiwz26k.execute-api.ap-southeast-1.amazonaws.com';
 
 const axiosInstance = axios.create({
   baseURL,
